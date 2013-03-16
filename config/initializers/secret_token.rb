ae88048d84ff5393c30f9378dabb9c5917b78c5e
brogupta <bgupta91@gmail.com>
2013-03-16 18:45:11 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bharatsapp::Application.config.secret_token = '03a466efb327f2fe8d78b31f811c2494ed325e073182845d9e52041598bdd83e191863492f49712319f3fcd91c05dcffbeebf50c8e43c2e96d6b95e08f150507'
